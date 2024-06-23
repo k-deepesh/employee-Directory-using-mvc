2532a7a190c65ab3ebde3b9103adfc085b32ea00
K.Deepesh kumar <134934472+k-deepesh@users.noreply.github.com>
2024-06-23 11:16:46 +0530

    Update EmployeeRepository.java

--- main/java/com/pracApp/dao/EmployeeRepository.java
+++ main/java/com/pracApp/dao/EmployeeRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    // no additional methods required
+    
 }
